2a7135337864f8c6ce8dd29328db3fc8d0fb520c
garyn <gary.najjarian@gmail.com>
2023-03-24 19:52:25 -0400

    more client.py adjustments

--- client.py
+++ client.py
@@ -7,13 +7,13 @@ this will serve as the code that handles the client (the sender).
 def client(IP, PORT, ADDR, FORMAT, SIZE):
     """ Staring a TCP socket. """
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    endpoint = input('Please enter the server\'s IP address.\n')
+    server_ip = input('Please enter the server\'s IP address.\n')
 
     """ attempt to connect to the server. """ 
     try:
-        client.connect(endpoint)
-    except:
-        print("Error connecting to server. Please try again.")
+        client.connect((server_ip, PORT))
+    except Exception as e:
+        print(f"Error connecting to server. Status code: {e}. Please try again.")
 
     """ Opening and reading the file data. """
     file_input = input("Please enter the filepath of desired file.")
@@ -31,8 +31,7 @@ def client(IP, PORT, ADDR, FORMAT, SIZE):
     print(f"[SERVER]: {msg}")
  
     """ Closing the file. """
-    file.close()
+    #file.close()
  
     """ Closing the connection from the server. """
     client.close()
- 
