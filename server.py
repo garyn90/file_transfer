a92f725230d0b5f8fbbeb9eb6a3e1c0cc6e0817e
garyn <gary.najjarian@gmail.com>
2023-05-05 05:46:07 -0400

    everything is working

--- server.py
+++ server.py
@@ -5,40 +5,6 @@ from os import path
 this will handle the endpoint listening to receive files. 
 """
 
-def server(IP, PORT, ADDR, SIZE, FORMAT):
-    print('Server has started.')
-    print(f'Your IP Address is: { IP }')
-
-    # initializing socket 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
-
-    while True:
-        conn, addr = server.accept()
-        print('New connection established')
-
-        # accept any incoming file per FORMAT and sent via SIZE 
-
-        # get the filename 
-        filename = conn.recv(SIZE).decode(FORMAT)
-        file = open(filename, 'w')
-        conn.send('Filename received.').encode(FORMAT)
-
-        # now get the data 
-        data = conn.recv(SIZE).decode(FORMAT)
-        print("Receiving file data...")
-        file.write(data)
-        conn.send("File received.")
-
-        # don't leave file-like object open
-        file.close()
-
-        # close the socket 
-        conn.close()
-        print('Connection ended.')
-
-
 class Server():
     def __init__(self, IP, PORT, ADDR, SIZE, FORMAT):
         # attributes to be passed in by computer
@@ -62,11 +28,8 @@ class Server():
                 continue 
             else:
                 break 
-        print('Listening for connections. To stop listening press Q to quit.')
+        print('Listening for connections. To stop listening press CNTRL-C to quit.')
         self.socket.listen(5)
-        # offer input to cleanly close the program
-        if input() == 'q':
-            quit()
         while True:
             conn, address = self.socket.accept()
             print(f'New connection established: {address}')
